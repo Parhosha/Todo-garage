f74cef99f6bc84caa484d4b65621651068707bb9
parhosha <parhosha@>
2020-10-28 00:18:02 +0200

    Updates

---
+++ precache-manifest.c4eab4bd28ba391dfbafa680f74830b7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a49c036e8ebb1bb393a23cab06eda72",
+    "url": "/Todo-garage/index.html"
+  },
+  {
+    "revision": "1003807b1943717ee8ac",
+    "url": "/Todo-garage/static/css/main.c764fd4b.chunk.css"
+  },
+  {
+    "revision": "03d1ee4b6cc46300b6ca",
+    "url": "/Todo-garage/static/js/2.f1331a6e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Todo-garage/static/js/2.f1331a6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1003807b1943717ee8ac",
+    "url": "/Todo-garage/static/js/main.f7c3e1ed.chunk.js"
+  },
+  {
+    "revision": "a84f1643b88a264c5e0d",
+    "url": "/Todo-garage/static/js/runtime-main.9cd35c92.js"
+  },
+  {
+    "revision": "13cf783b761ceba9199d93ec4cff647d",
+    "url": "/Todo-garage/static/media/1.13cf783b.png"
+  },
+  {
+    "revision": "829d0f9a12787f20103207eb46cd817e",
+    "url": "/Todo-garage/static/media/2.829d0f9a.png"
+  },
+  {
+    "revision": "2c2499f696ca521be00829f1e0ea6934",
+    "url": "/Todo-garage/static/media/3.2c2499f6.png"
+  }
+]);
